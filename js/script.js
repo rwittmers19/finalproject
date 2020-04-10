a91f1de14aab59c1b9dcc4786e5d7a807ecabee6
rwittmers191 <rwittmers19@georgefox.edu>
2020-04-10 08:55:51 -0700

    Working form!
    Added ajax call to free web host to make a working form!

--- js/script.js
+++ js/script.js
@@ -54,6 +54,7 @@ const showRequiredCategory = event => {
         }
     }
 
+    // Adds active class to images to display them and hide the others
     event.classList.add('active');
     const getCategory = document.querySelector(`.category-${getId}`);
     const categories = document.querySelectorAll('div[class ^= "category-"]');
@@ -69,4 +70,48 @@ const showRequiredCategory = event => {
     getCategory.classList.add('showCategory');
 }
 
-// Make the form make an AJAX call
+// Ajax Call for form!
+window.addEventListener("DOMContentLoaded", function() {
+
+    // Get the form elements defined in the form
+    
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+      status.innerHTML = "One or more of your fields was filled out incorrectly.";
+    }
+
+    // Handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // Helper function for sending an AJAX request
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
