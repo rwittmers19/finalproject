6d1a1f6bcae84fd2d34a8e2f72a12f844d27737e
ryan <rwittmers19@georgefox.edu>
2020-04-17 16:41:15 -0700

    finished API

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Final Project for CS304 at GFU. Pretty happy with how it came out!
 
 04/10/2020
 - Added functionality to the form to make it fully functional!
+
+04/17/2020
+- Added weather API
--- js/script.js
+++ js/script.js
@@ -70,6 +70,64 @@ const showRequiredCategory = event => {
     getCategory.classList.add('showCategory');
 }
 
+// Weather API
+const api = {
+  key: 'e94e8f1d190f2e4bd83285f4eed69b29',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(event) {
+  if (event.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`).then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults (weather) {
+  console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp).toFixed(0)}<span>°F</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hiLow = document.querySelector('.hi-low');
+  hiLow.innerText = `${Math.round(weather.main.temp_min)} °F / ${Math.round(weather.main.temp_max)} °F`;
+}
+
+function dateBuilder (d) {
+  let months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ]
+  let days = [
+    "Sunday", "Monday", "Tuesday",
+    "Wednesday", "Thursday", "Friday", "Saturday"
+  ]
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${month} ${date}, ${year}`;
+}
 // Ajax Call for form!
 window.addEventListener("DOMContentLoaded", function() {
 
