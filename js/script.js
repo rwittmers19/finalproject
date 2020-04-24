c9ecafed89a0a4a2e8e5fbd559689f26eb22cf57
rwittmers191 <rwittmers19@georgefox.edu>
2020-04-24 11:45:06 -0700

    trying to fix back to top button

--- js/script.js
+++ js/script.js
@@ -8,24 +8,6 @@ spans.forEach(span => span.addEventListener('mouseout', function(event) {
     span.classList.remove('animated','rubberBand');
 }));
 
-function SmoothVerticalScrolling(e, time, where) {
-  var eTop = e.getBoundingClientRect().top;
-  var eAmt = eTop / 100;
-  var curTime = 0;
-  while (curTime <= time) {
-      window.setTimeout(SVS_B, curTime, eAmt, where);
-      curTime += time / 100;
-  }
-}
-
-function SVS_B(eAmt, where) {
-  if(where == "center" || where == "")
-      window.scrollBy(0, eAmt / 2);
-  if (where == "top")
-      window.scrollBy(0, eAmt);
-}
-
-
 const backToTopBtn = document.querySelector("#back-to-top");
 window.addEventListener("scroll", scrollerFunction);
 
